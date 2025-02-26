4d692c3c91e53050cff53c1c0fe80ca936d148ac
lxlnihao <616231405@qq.com>
2025-02-26 14:40:03 +0800

    1

--- src/task.js
+++ src/task.js
@@ -14,7 +14,7 @@ function startSchedule(){
 function sendPush(){
 
   const sendStr = '推送的内容'
-  const pushStr = `https://api.day.app/APZB3y4mjyP5KyrQchwdXc/${sendStr}`
+  const pushStr = `https://api.day.app/APZB3y4mjyP5KyrQchwdXc/推送内容`
   http.get(pushStr, (req,res) => {
     
     res.send('success')
