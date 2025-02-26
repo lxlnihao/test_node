9ab595e38c79bcea4bd5cfe62d530ed0b891040b
lxlnihao <616231405@qq.com>
2025-02-26 14:48:00 +0800

    1

--- index.js
+++ index.js
@@ -14,15 +14,13 @@ http.get('/nihao', (req, res) => {
 schedule.scheduleJob('*/9 * * * * *', ()=>{
 
   console.log('每9秒执行一次')
-  this.sendPush()
-})
-function sendPush(){
   const pushStr = `https://api.day.app/APZB3y4mjyP5KyrQchwdXc/推送内容`
   http.get(pushStr, (req,res) => {
     
     res.send('success')
   }) 
-}
+})
+ 
  
 http.listen(3000, () => {
 
