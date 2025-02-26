00b6eb0a8e8d1e29b45dd4bed83a5268e6d8f858
lxlnihao <616231405@qq.com>
2025-02-26 14:45:26 +0800

    1

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 
 
-const {startSchedule} = require('./src/task')
-
 const {http} = require('./src/http')
+const schedule = require('node-schedule')
 
-  startSchedule()
 
   http.get('/', (req, res) => {
   res.send('Hello World!根目录')
@@ -12,6 +10,20 @@ const {http} = require('./src/http')
 http.get('/nihao', (req, res) => {
   res.send('Hello World!/nihao')
 })
+
+schedule.scheduleJob('*/9 * * * * *', ()=>{
+
+  console.log('每9秒执行一次')
+  this.sendPush()
+})
+function sendPush(){
+  const pushStr = `https://api.day.app/APZB3y4mjyP5KyrQchwdXc/推送内容`
+  http.get(pushStr, (req,res) => {
+    
+    res.send('success')
+  }) 
+}
+ 
 http.listen(3000, () => {
 
   console.log('web server running on port 3000')
